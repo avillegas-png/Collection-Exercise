261d1d84814b8c64db73e2ea8a6b455f60a0638c
avillegas-png <67976032+avillegas-png@users.noreply.github.com>
2020-07-15 14:22:01 -0500

    Add files via upload

---
+++ Alan_V_CollectionExercise.playground/Contents.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+var sockCollection: [String] = ["Black Socks", "White Socks", "Adidas Socks", "Nike Socks", "Puma Socks"]
+sockCollection.append("Reebok Socks")
+
+print (sockCollection)
