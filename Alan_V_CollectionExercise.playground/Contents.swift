bd3fe73efc17352287e1a38076cea1b8032bc208
avillegas-png <67976032+avillegas-png@users.noreply.github.com>
2020-07-16 22:49:59 -0500

    Add files via upload

--- Alan_V_CollectionExercise.playground/Contents.swift
+++ Alan_V_CollectionExercise.playground/Contents.swift
@@ -3,4 +3,7 @@ import UIKit
 var sockCollection: [String] = ["Black Socks", "White Socks", "Adidas Socks", "Nike Socks", "Puma Socks"]
 sockCollection.append("Reebok Socks")
 
+sockCollection.sort()
+
 print (sockCollection)
+
